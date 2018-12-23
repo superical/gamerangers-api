272765d3e29705002c0d9a4d742a77280b32dd22
superical <fluidliq@gmail.com>
2018-12-24 00:29:36 +0800

    Add authentication and registration with Google

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const User = require('../models').User
 const StatusCodeError = require('../helpers/StatusCodeError')
+const uuid = require('uuid/v4')
+const sha256 = require('sha256')
 
 const localStrategy = new LocalStrategy({
 	usernameField: 'email',
@@ -15,12 +18,34 @@ const localStrategy = new LocalStrategy({
 		.then(user => {
 			if(!user) return done(new StatusCodeError('Invalid account.', 400), false)
 			if(user.password !== password) return done(new StatusCodeError('Invalid email and password combination.', 400), false)
-			req.user = user
 			return done(null, user)
 		})
 		.catch(done)
 })
 
+const googleStrategy = new GoogleStrategy({
+	clientID: process.env.AUTH_GOOGLE_CLIENT_ID,
+	clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+	callbackURL: process.env.AUTH_GOOGLE_CALLBACK
+}, (token, refreshToken, profile, done) => {
+	User.findOne({where: {email: profile.emails[0].value}})
+		.then(user => {
+			if(!user) {
+				return User.create({
+					first_name: profile.name.givenName,
+					last_name: profile.name.familyName,
+					email: profile.emails[0].value,
+					password: sha256(uuid()),
+					google_id: profile.id
+				})
+			}
+			return user
+		})
+		.then(user => done(null, user))
+		.catch(done)
+})
+
 module.exports = {
-	localStrategy
+	localStrategy,
+	googleStrategy
 }
--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 const express = require('express')
 const path = require('path')
 const bodyparser = require('body-parser')
-const session = require('express-session')
 const passport = require('passport')
 const passportConfig = require('./config/passport')
-
-passport.use(passportConfig.localStrategy)
 
 const db = require('./helpers/db-connection')
 db.authenticate()
 	.then(() => console.log('Database connection has been established successfully.'))
 	.catch(err => console.error('Unable to connect to the database:', err))
 
-const gamesRoute = require('./controllers/GameController')
-const newsRoute = require('./routes/news')
-const usersRoute = require('./controllers/UserController')
-
-
 const app = express()
-var host = "127.0.0.1";
-var port = 8080;
+const host = "127.0.0.1";
+const port = 8080;
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: false}))
+
+app.engine('html', require('mustache-express')());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/public');
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(require('./middlewares/authenticate').optional)
+passport.use(passportConfig.localStrategy)
+passport.use(passportConfig.googleStrategy)
 app.use(passport.initialize());
 
 app.get('/', function(req, res) {
     res.send('hello3');
 });
-
-/*app.use('/games', gamesRoute)
-app.use('/news', newsRoute)
-app.use('/users', usersRoute)*/
 
 app.use('/', require('./routes/api'))
 
@@ -44,9 +39,9 @@ app.use(function(err, req, res, next) {
 	res.status(err.statusCode).json({error: err.message})
 });
 
-var server = app.listen(port, host, function() {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(port, host, function() {
+	const host = server.address().address;
+	const port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port);
 });
