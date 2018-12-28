718a5c27fba7bfa2d1e7309a69a84f593921c7c0
superical <fluidliq@gmail.com>
2018-12-28 12:34:08 +0800

    Fix validateRequestParams issue when parameter object is undefined

--- helpers/validateRequestParams.js
+++ helpers/validateRequestParams.js
@@ -1,6 +1,7 @@
 const StatusCodeError = require('./StatusCodeError')
 
 module.exports = (requiredParamsList, requestParamsObj) => {
+	if(requiredParamsList.length > 0 && requestParamsObj === undefined) throw new StatusCodeError('No parameters were provided.', 422)
 	requiredParamsList.forEach(param => {
 		if(!(param in requestParamsObj)) throw new StatusCodeError(`The ${param} field is required.`, 422)
 	})
