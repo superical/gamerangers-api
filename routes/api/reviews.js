7b54330a029c1f7c2370439c51a3832e472ada13
superical <fluidliq@gmail.com>
2018-12-30 00:43:05 +0800

    Fix reviews authentication access issue

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -2,11 +2,12 @@ const router = require('express').Router()
 const auth = require('../../middlewares/authenticate')
 
 const Review = require('../../models').Review
+const User = require('../../models').User
 const ReviewController = require('../../controllers/ReviewController')
 
 router.get('/', ReviewController.index)
 router.get('/:reviewid', ReviewController.viewById)
-router.post('/', auth.required, ReviewController.create)
+router.post('/', auth.selfOf(User, 'user_id', 'body'), ReviewController.create)
 router.patch('/:reviewid', auth.selfOf(Review, 'reviewid'), ReviewController.update)
 router.delete('/:reviewid', auth.selfOf(Review, 'reviewid'), ReviewController.remove)
 
