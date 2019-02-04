df95cbf598cb5f56d538428232f68719b88b0b45
superical <fluidliq@gmail.com>
2019-02-04 22:50:43 +0800

    Fix deprecated warning of send() to sendStatus() in CORS middleware

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = (req, res,next) => {
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS, HEAD')
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept')
 
-	if (req.method.toUpperCase() === 'OPTIONS') res.send(200)
+	if (req.method.toUpperCase() === 'OPTIONS') res.sendStatus(200)
 	else next()
 }
 
