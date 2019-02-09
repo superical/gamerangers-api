6959fd5d4d18050262e41ea929e41d62b29e8078
superical <fluidliq@gmail.com>
2019-02-09 23:40:49 +0800

    Add update profile information modal box form to frontend

--- public/js/profile.js
+++ public/js/profile.js
@@ -13,7 +13,42 @@ const getCurrentUserInfo = () => {
                 profileIsAdmin.innerHTML = 'Yes, user is an administrator.'
             else
                 profileIsAdmin.innerHTML = 'No, user is just a normal member.'
+
+	        const profileFirstNameInput = document.querySelector('#profileFirstNameInput')
+	        const profileLastNameInput = document.querySelector('#profileLastNameInput')
+	        const profileEmailInput = document.querySelector('#profileEmailInput')
+	        profileFirstNameInput.value = userData.first_name
+	        profileLastNameInput.value = userData.last_name
+	        profileEmailInput.value = userData.email
         })
+}
+
+const initUpdateProfileInfoForm = () => {
+	const updateProfileInfoForm = document.querySelector('#updateProfileInfoForm')
+	updateProfileInfoForm.addEventListener('submit', function(e) {
+		e.preventDefault()
+	    if(
+	        this.elements['profileFirstNameInput'].value.trim() === '' ||
+	        this.elements['profileLastNameInput'].value.trim() === '' ||
+	        this.elements['profileEmailInput'].value.trim() === ''
+        ) {
+	        alert('You should not have any empty fields for profile information.')
+            return
+        }
+		Utils.fetchAuthApi(`${Configuration.apiUrl}/users/${Authentication.getAuthInfo().id}`, 'PATCH', {
+			first_name: this.elements['profileFirstNameInput'].value,
+			last_name: this.elements['profileLastNameInput'].value,
+			email: this.elements['profileEmailInput'].value
+		})
+			.then(res => res.json())
+            .then(resData => {
+                if(resData.error) throw new Error(resData.error)
+                alert('Profile updated successfully!')
+	            $('#updateProfileModalBox').modal('hide')
+	            window.location.reload()
+            })
+            .catch(err => alert(`An error has occurred:\n${err.message}`))
+    })
 }
 
 window.addEventListener('load', function(e) {
@@ -23,5 +58,6 @@ window.addEventListener('load', function(e) {
     } else {
         document.querySelector('#profileContainer').style.display = 'flex'
         getCurrentUserInfo()
+	    initUpdateProfileInfoForm()
     }
 })
